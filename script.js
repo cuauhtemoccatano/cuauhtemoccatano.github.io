d9a3897f8c0760e18bf624da5c46e4a558429c94
Cuauhtémoc Cataño De Pavía <70673042+cuauhtemoccatano@users.noreply.github.com>
2023-12-26 15:41:07 -0600

    minor improvements

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ cloneContainer.classList.remove("active");
 const toggleIcons = document.querySelectorAll(".toggle-icon");
 const icons = document.querySelectorAll(".toggle-icon i");
 const darkContainer = document.querySelector("#dark-container");
-
 const darkContainerImg = document.querySelector(
   "#dark-container .home-img img"
 );
@@ -17,6 +16,11 @@ darkContainerImg.src = "Assets/headshotbw.png";
 
 toggleIcons.forEach((toggle) => {
   toggle.addEventListener("click", () => {
+    toggle.classList.add("disabled");
+    setTimeout(() => {
+      toggle.classList.remove("disabled");
+    }, 1500);
+
     icons.forEach((icon) => {
       icon.classList.toggle("bx-sun");
     });
