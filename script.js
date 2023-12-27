17fd13e40fc71faf7d8010f2a4af5ef2d3897235
Cuauhtémoc Cataño De Pavía <70673042+cuauhtemoccatano@users.noreply.github.com>
2023-12-26 21:50:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let container = document.querySelector(".container");
+const container = document.querySelector(".container");
 
 const cloneContainer = container.cloneNode(true);
 cloneContainer.id = "dark-container";
